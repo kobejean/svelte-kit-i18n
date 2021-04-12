23f0c61d97d421d2669e3439c5c9bf8153b89271
Jean Atsumi Flaherty <kobejean@me.com>
2021-04-12 17:41:51 -0600

    svelte-i18n doesn't work

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,12 +1,12 @@
 <script context="module">
-	import { preloadLocale } from '$lib/utils/i18n/preload';
-	import { waitLocale } from 'svelte-i18n';
+	// import { preloadLocale } from '$lib/utils/i18n/preload';
+	// import { waitLocale } from 'svelte-i18n';
 	/**
 	 * @type {import('@sveltejs/kit').Load}
 	 */
 	export async function load({ page, fetch, session, context }) {
-		await preloadLocale(fetch, page, session);
-		await waitLocale();
+		// await preloadLocale(fetch, page, session);
+		// await waitLocale();
 		return {
 			props: {
 				locale: session.locale
